85b4fbf3cbdb08714281330f4d1d5d212f8d9aac
hasanie <hasanienag237@gmail.com>
2023-09-18 16:01:46 +0530

    added test cases

---
+++ Tests/HomeControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace SimpleApp.Tests
+{
+    public class HomeControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public HomeControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task HomePage_ReturnsHelloWorldMessage()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            Assert.Contains("Hello, World!", responseString);
+        }
+    }
+}

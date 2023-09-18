28a831aaf828dc77f691a5b353e4f340c7408ff9
Your Name <hasanieliyanagama@LK-65NFC63>
2023-09-18 14:28:11 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleApp.Models;
+using System.Diagnostics;
+
+namespace SimpleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Hello, World!";
+            return View();
+        }
+    }
+}
